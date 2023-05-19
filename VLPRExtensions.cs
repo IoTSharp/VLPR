705d0c53d33caf64a9ed3209852918245898bd1f
MysticBoy <mysticboy@live.com>
2023-05-19 22:15:48 +0800

    加入健康检查

--- VLPRExtensions.cs
+++ VLPRExtensions.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using System;
+﻿using System;
 using System.Text.RegularExpressions;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -17,8 +14,18 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddOptions<VLPROptions>().BindConfiguration(nameof(VLPROptions));
             services.AddHostedService<VLPRService>();
             services.AddSingleton<VLPRClient>();
+        
         }
-     
+        /// <summary>
+        /// 加入健康检查
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static IHealthChecksBuilder AddVLPR(this IHealthChecksBuilder builder,string name=null)
+        {
+            return builder.AddTypeActivatedCheck<VLPRHealthCheck>(name??"VLPR");
+        }
     }
 }
 namespace System.Text
---
+++ VLPRHealthCheck.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using Microsoft.Extensions.Options;
+
+public class VLPRHealthCheck : IHealthCheck
+{
+    private readonly VLPROptions options;
+    private readonly VLPRClient client;
+
+    public VLPRHealthCheck(IOptions<VLPROptions> options, VLPRClient client)
+
+    {
+        this.options = options.Value;
+        this.client = client;
+    }
+
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        List<(string name, bool ok)> lst = new List<(string name, bool ok)>();
+        options.VLPRConfigs.ForEach(config =>
+        {
+            var ok = client.CheckStatus(config.Name);
+            lst.Add((config.Name, ok));
+        });
+        if (lst.All(f => !f.ok))
+        {
+            return Task.FromResult(HealthCheckResult.Unhealthy(description: string.Join(";", lst.Select(c => $"车道:{c.name} {(c.ok ? "正常" : "故障")}").ToList())));
+        }
+        else if (lst.Any(f => !f.ok))
+        {
+            return Task.FromResult(HealthCheckResult.Degraded(description: string.Join(";", lst.Select(c => $"车道:{c.name} {(c.ok ? "正常" : "故障")}").ToList())));
+        }
+        else
+        {
+            return Task.FromResult(HealthCheckResult.Healthy());
+        }
+    }
+}
